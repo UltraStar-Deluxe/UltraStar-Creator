49da05081af506a0eb6bd361c0319eea766d85e9
bohning <bohning@gmx.net>
2012-03-22 12:11:53 +0000

    defined constants for encodings and music signs (flat, sharp)

--- src/QU.h
+++ src/QU.h
@@ -10,6 +10,13 @@
 #define CHAR_UTF8_APPROX "\xe2\x89\x88"
 #define CHAR_UTF8_NEQUAL "\xe2\x89\xa0"
 #define CHAR_UTF8_DOT    "\xc2\xb7"
+#define CHAR_UTF8_SHARP  "\xe2\x99\xaf"
+#define CHAR_UTF8_FLAT   "\xe2\x99\xad"
+
+// used for encodings
+#define ENCODING_CP1250	"CP1250"
+#define ENCODING_CP1252	"CP1252"
+#define ENCODING_UTF8	"UTF8"
 
 //! This class provides global flags and enumerations.
 class QU: QObject {
