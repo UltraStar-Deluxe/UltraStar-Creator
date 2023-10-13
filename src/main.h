ee949b23f1c0ca42ed126054273fbfa4ac0b4546
Markus Böhning <1497707+bohning@users.noreply.github.com>
2023-10-13 16:47:54 +0200

    Bump MINOR_VERSION to 3.

--- src/main.h
+++ src/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H_
 
 #define MAJOR_VERSION 0x0001
-#define MINOR_VERSION 0x0002
+#define MINOR_VERSION 0x0003
 #define PATCH_VERSION 0x0000
 
 #define WIP_TEXT "" // " *work in progress* -> Please do not distribute."
